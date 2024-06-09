c02ae50b83067f9da9e13bd7d1fd018fd9b39133
facundoms284 <faccumalacrida09@gmail.com>
2024-06-09 19:42:31 -0300

    feat: :art: Terminé la funcionalidad principal del generador de colores.

--- main.js
+++ main.js
@@ -1,70 +1,160 @@
 const mainContainer = document.getElementById('main');
 
 let colors = [
-    obtenerColorHexadecimal(),
-    obtenerColorHexadecimal(),
-    obtenerColorHexadecimal(),
+    {
+        fijo: false,
+        color: obtenerColorHexadecimal(),
+    },
+    {
+        fijo: false,
+        color: obtenerColorHexadecimal(),
+    },
+    {
+        fijo: false,
+        color: obtenerColorHexadecimal(),
+    },
 ];
 
-const chequearLimites = (() => {
+const coloresGuardados = localStorage.getItem('colors');
+
+if (coloresGuardados) {
+    //Convierto nuevamente el string a un array de objetos
+    const arrayDeObjetosColors = JSON.parse(coloresGuardados);
+    if (arrayDeObjetosColors.length > 0) {
+        colors = arrayDeObjetosColors;
+    };
+};
+
+
+const guardarColores = () => {
+    const coloresString = JSON.stringify(colors);
+    localStorage.setItem('colors', coloresString);
+};
+
+const chequearLimite = () => {
     const maxColors = 6;
+    const minColors = 2;
     const size = colors.length;
+
     if (size >= maxColors) {
-        const buttonContainers = document.getElementsByClassName('button-container');
-        const containers = Array.from(buttonContainers);
-        containers.forEach((container) => {
+            const elementosHTMl = document.getElementsByClassName('button-container');
+            const arrayDeDivs = Array.from(elementosHTMl);
+            arrayDeDivs.forEach((container) => {
             container.style.display = 'none';
         });
-    }
-})
+    };
 
+    if (size <= minColors) {
+        const elementosHTML = document.getElementsByClassName('button-delete');
+        const arrayDeDeleteButtons = Array.from(elementosHTML);
+        arrayDeDeleteButtons.forEach((button) => {
+            button.style.display = 'none';
+        });
+    };
+};
 
-const pintarContenedor = (color, index) => {
-    //Creo un div por cada color.
-    const div = document.createElement('div');
-    //A cada div le asigno un bg color.
-    div.style.backgroundColor = `#${color}`;
+const agregarColor = (posicion) => {
+    const nuevoColor = {
+        fijo: false,
+        color: obtenerColorHexadecimal(),
+    };
     
-    //Creo un h1
-    const h1 = document.createElement('h1');
-    //A cada h1 le cambio el texto a el color hexadecimal.
-    h1.textContent = `#${color}`;
-    //Agrego el h1 al div.
-    div.appendChild(h1);
+    colors.splice(posicion, 0, nuevoColor);
+    pintarColores();
+};
 
+const obtenerBotonAgregar = (posicion) => {
     const buttonContainer = document.createElement('div');
     buttonContainer.classList.add('button-container');
+
     const button = document.createElement('button');
+    button.classList.add('button-add');
     button.textContent = '+';
-
-    button.addEventListener('click', function () {
-        const newColor = obtenerColorHexadecimal();
-        colors.splice(index + 1, 0, newColor);
-        pintarColores();
-        chequearLimites();
-    });
-
-    div.appendChild(buttonContainer);
     buttonContainer.appendChild(button);
-
-    //A cada div le asigno la class container
-    div.classList.add('container');
     
-    //Agrego cada div a la section main.
+    button.addEventListener('click', () => agregarColor(posicion + 1));
+    return buttonContainer;
+};
+
+const obtenerBotonEliminar = (posicion) => {
+
+    const button = document.createElement('button');
+    button.classList.add('button-delete');
+    const img = document.createElement('img');
+    img.src = './utils/images/delete-color.svg';
+    button.appendChild(img);
+    
+    button.addEventListener('click', () => {
+        colors.splice(posicion, 1);
+        pintarColores();
+    });
+    return button;
+};
+
+const obtenerBotonDejarFijo = (posicion) => {
+    const button = document.createElement('button');
+    button.classList.add('button-set');
+    const img = document.createElement('img');
+    img.src = `${colors[posicion].fijo ? './utils/images/unlock-color' : './utils/images/lock'}.svg`;
+    button.appendChild(img);
+    
+    button.addEventListener('click', () => {
+        colors[posicion].fijo = !colors[posicion].fijo;
+        button.innerHTML = `<img src="${colors[posicion].fijo ? './utils/images/unlock-color' : './utils/images/lock'}.svg">`;
+
+        guardarColores();
+    });
+    return button;
+};
+    
+const pintarContenedor = (color, posicion) => {
+
+    //Creo los divs que voy a usar para mostrar los colores.
+    const div = document.createElement('div');
+    div.classList.add('container');
+    div.style.backgroundColor = `#${color}`;
+
+    const h1 = document.createElement('h1');
+    h1.textContent = `#${color}`;
+
+    const botonAñadirColor = obtenerBotonAgregar(posicion);
+    const botonEliminarColor = obtenerBotonEliminar(posicion);
+    const botonDejarFijo = obtenerBotonDejarFijo(posicion);
+
+    div.appendChild(h1);
+    div.appendChild(botonAñadirColor);
+    div.appendChild(botonEliminarColor);
+    div.appendChild(botonDejarFijo);
+
+    //Agrego los divs a la section main.
     mainContainer.appendChild(div);
-}
+};
+
 
 const pintarColores = () => {
     mainContainer.innerHTML = '';
+    
+    colors.forEach((item, index) => {
+        pintarContenedor(item.color, index);
+    });
 
-    colors.forEach(pintarContenedor)
-    chequearLimites();
+    chequearLimite();
+    guardarColores();
 };
 
 const pintarColoresAleatorios = () => {
-    colors = colors.map(obtenerColorHexadecimal);
+    colors = colors.map((item) => {
+        if (item.fijo) {
+            return item;
+        }
+        return {
+            fijo: false,
+            color: obtenerColorHexadecimal(),
+        }
+
+    });
     pintarColores();
-}
+};
 
 document.addEventListener('keyup', function (event) {
     if (event.code === 'Space') {
